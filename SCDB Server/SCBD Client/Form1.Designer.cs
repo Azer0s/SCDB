9f48220f2e9cd8b6906119c3b5da8bc87bf0000c
Azer0s <sim17057@spengergasse.at>
2016-11-01 13:10:19 +0100

    Changed client UI

--- SCDB Server/SCBD Client/Form1.Designer.cs
+++ SCDB Server/SCBD Client/Form1.Designer.cs
@@ -51,16 +51,14 @@ namespace SCBD_Client
             // 
             // tabControl1
             // 
-            this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
+            this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.tabControl1.Controls.Add(this.tabPage1);
             this.tabControl1.Controls.Add(this.tabPage2);
             this.tabControl1.Location = new System.Drawing.Point(0, 0);
-            this.tabControl1.Margin = new System.Windows.Forms.Padding(4);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(652, 107);
+            this.tabControl1.Size = new System.Drawing.Size(489, 87);
             this.tabControl1.TabIndex = 0;
             // 
             // tabPage1
@@ -69,28 +67,29 @@ namespace SCBD_Client
             this.tabPage1.Controls.Add(this.button3);
             this.tabPage1.Controls.Add(this.button1);
             this.tabPage1.Controls.Add(this.textBox1);
-            this.tabPage1.Location = new System.Drawing.Point(4, 25);
-            this.tabPage1.Margin = new System.Windows.Forms.Padding(4);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
             this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(4);
-            this.tabPage1.Size = new System.Drawing.Size(644, 78);
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3, 3, 3, 3);
+            this.tabPage1.Size = new System.Drawing.Size(481, 61);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Statement";
             this.tabPage1.UseVisualStyleBackColor = true;
             // 
             // cn1
             // 
-            this.cn1.Location = new System.Drawing.Point(218, 42);
+            this.cn1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.cn1.Location = new System.Drawing.Point(164, 34);
+            this.cn1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.cn1.Name = "cn1";
-            this.cn1.Size = new System.Drawing.Size(410, 22);
+            this.cn1.Size = new System.Drawing.Size(308, 20);
             this.cn1.TabIndex = 3;
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(119, 39);
-            this.button3.Margin = new System.Windows.Forms.Padding(4);
+            this.button3.Location = new System.Drawing.Point(89, 32);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(92, 28);
+            this.button3.Size = new System.Drawing.Size(69, 23);
             this.button3.TabIndex = 2;
             this.button3.Text = "Connect";
             this.button3.UseVisualStyleBackColor = true;
@@ -98,10 +97,9 @@ namespace SCBD_Client
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(11, 39);
-            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Location = new System.Drawing.Point(8, 32);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(100, 28);
+            this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 1;
             this.button1.Text = "Declare";
             this.button1.UseVisualStyleBackColor = true;
@@ -112,10 +110,9 @@ namespace SCBD_Client
             this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBox1.Location = new System.Drawing.Point(11, 7);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Location = new System.Drawing.Point(8, 6);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(617, 22);
+            this.textBox1.Size = new System.Drawing.Size(464, 20);
             this.textBox1.TabIndex = 0;
             // 
             // tabPage2
@@ -124,28 +121,29 @@ namespace SCBD_Client
             this.tabPage2.Controls.Add(this.button4);
             this.tabPage2.Controls.Add(this.button2);
             this.tabPage2.Controls.Add(this.textBox2);
-            this.tabPage2.Location = new System.Drawing.Point(4, 25);
-            this.tabPage2.Margin = new System.Windows.Forms.Padding(4);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
             this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(4);
-            this.tabPage2.Size = new System.Drawing.Size(644, 78);
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3, 3, 3, 3);
+            this.tabPage2.Size = new System.Drawing.Size(481, 61);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Question";
             this.tabPage2.UseVisualStyleBackColor = true;
             // 
             // cn
             // 
-            this.cn.Location = new System.Drawing.Point(218, 42);
+            this.cn.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.cn.Location = new System.Drawing.Point(164, 34);
+            this.cn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.cn.Name = "cn";
-            this.cn.Size = new System.Drawing.Size(410, 22);
+            this.cn.Size = new System.Drawing.Size(308, 20);
             this.cn.TabIndex = 4;
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(119, 39);
-            this.button4.Margin = new System.Windows.Forms.Padding(4);
+            this.button4.Location = new System.Drawing.Point(89, 32);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(92, 28);
+            this.button4.Size = new System.Drawing.Size(69, 23);
             this.button4.TabIndex = 3;
             this.button4.Text = "Connect";
             this.button4.UseVisualStyleBackColor = true;
@@ -153,10 +151,9 @@ namespace SCBD_Client
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(11, 39);
-            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Location = new System.Drawing.Point(8, 32);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(100, 28);
+            this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 2;
             this.button2.Text = "Ask";
             this.button2.UseVisualStyleBackColor = true;
@@ -167,21 +164,23 @@ namespace SCBD_Client
             this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBox2.Location = new System.Drawing.Point(11, 7);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox2.Location = new System.Drawing.Point(8, 6);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(617, 22);
+            this.textBox2.Size = new System.Drawing.Size(464, 20);
             this.textBox2.TabIndex = 1;
             // 
             // dataGridView1
             // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.Result});
-            this.dataGridView1.Location = new System.Drawing.Point(0, 105);
-            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 6, 4, 4);
+            this.dataGridView1.Location = new System.Drawing.Point(0, 85);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(3, 5, 3, 3);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(652, 151);
+            this.dataGridView1.Size = new System.Drawing.Size(489, 101);
             this.dataGridView1.TabIndex = 1;
             // 
             // Result
@@ -192,12 +191,12 @@ namespace SCBD_Client
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(651, 256);
+            this.ClientSize = new System.Drawing.Size(488, 186);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.tabControl1);
-            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MinimumSize = new System.Drawing.Size(300, 200);
             this.Name = "Form1";
             this.Text = "SCDB Client";
             this.tabControl1.ResumeLayout(false);
